e66222e0c5a8d0cbe6fd0bd95deffd15f76df505
Murodjon Abdujabbor <murodjonabdujabbor33@gmail.com>
2022-11-13 22:28:00 +0500

    updated

--- Phone/smartphones/urls.py
+++ Phone/smartphones/urls.py
@@ -4,6 +4,6 @@ from .views import get_brands, get_models, get_products, HomaPageView
 urlpatterns = [
     path('', HomaPageView.as_view(), name='home'),
     path('brand/', get_brands, name='brands'),
-    path('brand/models', get_models, name='models'),
+    path('brand/<str:model_name>', get_models, name='models'),
     path('brand/model/<str:brand_name>', get_products, name='products'),
 ]
