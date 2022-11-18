4b14e8af4dc7ea86899da8b5434469beac1a1d6c
Murodjon Abdujabbor <murodjonabdujabbor33@gmail.com>
2022-11-18 23:20:37 +0500

    updated

--- Blog-1/blog/views.py
+++ Blog-1/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 # Create your views here.
@@ -12,4 +12,10 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
 
