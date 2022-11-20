a1793bacfbb32b7ee34dec46bf0fea6ad6f68a3d
Murodjon Abdujabbor <murodjonabdujabbor33@gmail.com>
2022-11-20 21:52:12 +0500

    updated

--- Blog-1/blog/views.py
+++ Blog-1/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -26,4 +27,5 @@ class BlogUpdateView(UpdateView):
 class BlogDeleteView(DeleteView):
     model = Post
     template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
 
