efec106725ddb92ff632e2d3fd72519018916470
Murodjon Abdujabbor <murodjonabdujabbor33@gmail.com>
2022-12-08 12:10:15 +0500

    updated

--- Library-project/api/views.py
+++ Library-project/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from books.models import Book
+from .serializers import BookSerializers # modeldagi ma'lumotlarni json ga o'tkazish
 
 # Create your views here.
+
+class BookApiView(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializers
