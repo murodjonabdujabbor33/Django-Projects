0e39072355af7e5f64cd1688a5c207525a5c106a
Murodjon Abdujabbor <murodjonabdujabbor33@gmail.com>
2022-12-08 11:55:04 +0500

    add api app

--- Library-project/config/settings.py
+++ Library-project/config/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # tashqi modullar
+    'rest_framework',
+
+    # local apps
     'books',
+    'api',
 ]
 
 MIDDLEWARE = [
