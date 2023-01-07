73826e54f2fd785b80ef6a83907d7493281c85b9
murodjonabdujabbor33 <murodjonabdujabbor33>
2023-01-07 11:24:31 +0500

    delete 1 file

--- BlogAPI/posts/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-class IsAuthorOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        # faqat ko'rish uchun ruxsat beriladi.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        # o'zgartirish ya'ni yozish uchun ruxsatnoma faqat post muallifiga beriladi.
-        return obj.author == request.user
--- BlogAPI/posts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PostList, PostDetail
-
-urlpatterns = [
-    path('<int:pk>/', PostDetail.as_view(), name='detail'),
-    path('', PostList.as_view(), name='home'),
-]
